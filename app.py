91f0ac6bd5b9852431bbc634d7a24fbe13148e02
Vinay Panchal <vinay.npanchal@gmail.com>
2022-01-23 20:35:41 +0530

    python basic complete

---
+++ app.py
@@ -0,0 +1,4 @@
+from main import User
+
+harsh = User(name="Harsh", age=25, salary=50000)
+harsh.display()
--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print("heelo world")
+class User:
+    def __init__(self, name, age, salary):
+        self.name = name
+        self.age = age
+        self.salary = salary
+
+    
+    def display(self):
+        print("Name: ", self.name)
+        print("Age: ", self.age)
+        print("Salary: ", self.salary)
+
+
+harsh = User()
+
+try:
+    harsh = User()
+except Exception as e:
+    print(e)
+else:
+    print("No exception occured")
+def count(start, step):
+    while True:
+        yield start
+        start += step
+
